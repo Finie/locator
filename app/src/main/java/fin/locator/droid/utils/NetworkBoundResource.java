010e0cabf667254835788d52da2f75518016ba30
unknown <finiephenius@gmail.com>
2020-11-21 11:47:03 +0300

    removing errors from the network bound resource class

--- app/src/main/java/fin/locator/droid/utils/NetworkBoundResource.java
+++ app/src/main/java/fin/locator/droid/utils/NetworkBoundResource.java
@@ -5,28 +5,26 @@ import android.os.Build;
 
 import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
+import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
 
 
-
+import java.util.Objects;
 
 import fin.locator.droid.remote.Resource;
 import fin.locator.droid.remote.persistence.ApiResponse;
 import fin.locator.droid.remote.persistence.AppExecutors;
 
-public abstract class NetworkBoundResource<ResultType,RequestType> {
+public abstract class NetworkBoundResource<ResultType, RequestType> {
+    private final AppExecutors appExecutors;
 
-    private final AppExecutors mAppExecutors;
+    private final MediatorLiveData<Resource<ResultType>> result = new MediatorLiveData<>();
 
-    private final MediatorLiveData result = new MediatorLiveData();
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @MainThread
-    public NetworkBoundResource(AppExecutors mAppExecutors) {
-        this.mAppExecutors = mAppExecutors;
+    public NetworkBoundResource(AppExecutors appExecutors) {
+        this.appExecutors = appExecutors;
         result.setValue(Resource.loading(null));
         LiveData<ResultType> dbSource = loadFromDb();
         result.addSource(dbSource, data -> {
@@ -37,35 +35,28 @@ public abstract class NetworkBoundResource<ResultType,RequestType> {
                 result.addSource(dbSource, newData -> setValue(Resource.success(newData)));
             }
         });
-
     }
 
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @MainThread
-    private void setValue(Resource<ResultType> newValue){
-        if (!Objects.equals(result.getValue(), newValue)){
-            result.setValue(newValue);
+    private void setValue(Resource<ResultType> newValue) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (!Objects.equals(result.getValue(), newValue)) {
+                result.setValue(newValue);
+            }
         }
     }
 
-
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void fetchFromNetwork(final LiveData<ResultType> dbSource){
-
-
+    private void fetchFromNetwork(final LiveData<ResultType> dbSource) {
         LiveData<ApiResponse<RequestType>> apiResponse = createCall();
         // we re-attach dbSource as a new source, it will dispatch its latest value quickly
         result.addSource(dbSource, newData -> setValue(Resource.loading(newData)));
         result.addSource(apiResponse, response -> {
             result.removeSource(apiResponse);
             result.removeSource(dbSource);
-            //noinspection ConstantConditions
             if (response.isSuccessful()) {
-                mAppExecutors.getmDiskIO().execute(() -> {
+                appExecutors.getmDiskIO().execute(() -> {
                     saveCallResult(processResponse(response));
-                    mAppExecutors.getmMainThread().execute(() ->
+                    appExecutors.getmMainThread().execute(() ->
                             // we specially request a new live data,
                             // otherwise we will get immediately last cached value,
                             // which may not be updated with latest results received from network.
@@ -75,29 +66,30 @@ public abstract class NetworkBoundResource<ResultType,RequestType> {
                 });
             } else {
                 onFetchFailed();
-                result.addSource(dbSource,
-                        newData -> setValue(Resource.error(response.errorMessage, newData)));
+                if (response.code == 401) {
+
+             /*       result.addSource(dbSource,
+                            newData -> setValue(Resource
+                                    .reAuthenticate(response.errorMessage, newData)));
+                    */
+
+
+
+                } else {
+                    result.addSource(dbSource,
+                            newData -> setValue(Resource
+                                    .error(response.errorMessage, newData)));
+                }
             }
         });
-
     }
 
-    protected  void onFetchFailed(){
+    protected void onFetchFailed() {
+    }
 
-    };
-
-    @NonNull
-    @MainThread
-    protected abstract LiveData<ApiResponse<RequestType>> createCall();
-
-    @NonNull
-    @MainThread
-    protected abstract LiveData<ResultType> loadFromDb();
-
-
-
-    protected abstract boolean shouldFetch(Object data);
-
+    public LiveData<Resource<ResultType>> asLiveData() {
+        return result;
+    }
 
     @WorkerThread
     protected RequestType processResponse(ApiResponse<RequestType> response) {
@@ -107,6 +99,15 @@ public abstract class NetworkBoundResource<ResultType,RequestType> {
     @WorkerThread
     protected abstract void saveCallResult(@NonNull RequestType item);
 
+    @MainThread
+    protected abstract boolean shouldFetch(@Nullable ResultType data);
 
+    @NonNull
+    @MainThread
+    protected abstract LiveData<ResultType> loadFromDb();
+
+    @NonNull
+    @MainThread
+    protected abstract LiveData<ApiResponse<RequestType>> createCall();
 
 }
